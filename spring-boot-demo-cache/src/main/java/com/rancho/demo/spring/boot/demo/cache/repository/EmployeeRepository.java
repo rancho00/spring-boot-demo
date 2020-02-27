2e26020c210c1647a20ca0a0693c199df971ca27
rancho <191405738@qq.com>
2020-02-27 17:06:07 +0800

    调整spring-boot-demo-cache目录

--- spring-boot-demo-cache/src/main/java/com/rancho/demo/spring/boot/demo/cache/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rancho.demo.spring.boot.demo.cache.repository;
-
-import com.rancho.demo.spring.boot.demo.cache.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-
-}
