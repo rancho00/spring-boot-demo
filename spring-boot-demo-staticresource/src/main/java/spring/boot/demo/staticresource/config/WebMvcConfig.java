af179c12c7fbf0bb54bd44b3ff2c9ed6ee9586de
rancho <191405738@qq.com>
2020-03-13 18:02:58 +0800

    修改staticresource文件结构

--- spring-boot-demo-staticresource/src/main/java/spring/boot/demo/staticresource/config/WebMvcConfig.java
+++ spring-boot-demo-staticresource/src/main/java/spring/boot/demo/staticresource/config/WebMvcConfig.java
@@ -4,11 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-//@Configuration
+@Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        //registry.addResourceHandler("/file/**").addResourceLocations("classpath:/file/","classpath:/file1/");
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/java2/");
     }
 }
