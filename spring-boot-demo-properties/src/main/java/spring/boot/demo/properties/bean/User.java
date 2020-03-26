822120f7382218244809a3e8342d237c3810ef00
rancho <191405738@qq.com>
2020-03-26 11:00:02 +0800

    新增spring-boot-demo-properties模块

---
+++ spring-boot-demo-properties/src/main/java/spring/boot/demo/properties/bean/User.java
@@ -0,0 +1,113 @@
+package spring.boot.demo.properties.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Email;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+@ConfigurationProperties(prefix = "user")
+@Validated
+public class User {
+    private String name;
+    private Integer age;
+    @Email
+    private String email;
+    private Date createTime;
+    private Boolean enable;
+    private Map<String,Object> map;
+    private List<Object> list;
+    private List<Dog> list1;
+    private Dog dog;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Boolean getEnable() {
+        return enable;
+    }
+
+    public void setEnable(Boolean enable) {
+        this.enable = enable;
+    }
+
+    public Map<String, Object> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, Object> map) {
+        this.map = map;
+    }
+
+    public List<Object> getList() {
+        return list;
+    }
+
+    public void setList(List<Object> list) {
+        this.list = list;
+    }
+
+    public List<Dog> getList1() {
+        return list1;
+    }
+
+    public void setList1(List<Dog> list1) {
+        this.list1 = list1;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", createTime=" + createTime +
+                ", enable=" + enable +
+                ", map=" + map +
+                ", list=" + list +
+                ", list1=" + list1 +
+                ", dog=" + dog +
+                '}';
+    }
+}
---
+++ spring-boot-demo-properties/src/test/java/spring/boot/demo/properties/PropertiesApplicationTests.java
@@ -0,0 +1,31 @@
+package spring.boot.demo.properties;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import spring.boot.demo.properties.bean.User;
+import spring.boot.demo.properties.bean.User1;
+import spring.boot.demo.properties.bean.User2;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PropertiesApplicationTests {
+
+    @Autowired
+    private User user;
+
+    @Autowired
+    private User1 user1;
+
+    @Autowired
+    private User2 user2;
+
+    @Test
+    public void contextLoads(){
+        //System.out.println(user);
+        //System.out.println(user1);
+        System.out.println(user2);
+    }
+}
