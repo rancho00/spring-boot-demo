1ef87d918364135085789280423d2e93e4ea85a2
rancho <191405738@qq.com>
2020-03-26 17:46:14 +0800

    新增spring-boot-demo-profile模块

---
+++ spring-boot-demo-profile/src/main/java/spring/boot/demo/profile/ProfileApplication.java
@@ -0,0 +1,12 @@
+package spring.boot.demo.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileApplication.class,args);
+    }
+}
